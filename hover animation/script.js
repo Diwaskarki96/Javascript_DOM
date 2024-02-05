53bb4a32cd79fe9b0a423e0ded1208004d908bf9
Diwash Karki <diwaskarki96@gmail.com>
2024-02-05 13:53:46 +0545

    some change

--- hover animation/script.js
+++ hover animation/script.js
@@ -7,7 +7,7 @@ elem.forEach((val) => {
   val.addEventListener("mouseleave", () => {
     val.childNodes[3].style.opacity = 0;
   });
-  val.addEventListener("mousemove", (value) => {
-    val.childNodes[3].style.left = value.x + "px";
+  val.addEventListener("mousemove", (item) => {
+    val.childNodes[3].style.left = item.x + "px";
   });
 });
